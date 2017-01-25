8f6aefd4ed7673595cb6868dd99c0370a0c857bb
Nishanth Nagendra <nishanth.amogh@gmail.com>
2017-01-25 12:17:19 +0530

    updates

--- c/stack/stack.c
+++ c/stack/stack.c
@@ -4,41 +4,82 @@
 
 #define STACK_SIZE 10
 
-static int array[STACK_SIZE];
-static int count_elem = -1;
+typedef struct 
+{
+	int size;
+	int *items;
+	int top;
+} stack;
+
+//static int array[STACK_SIZE];
+//static int count_elem = -1;
+
 
 static void
-push(int elem)
+_init_stack(stack *s, int size)
 {
-	if (count_elem == (STACK_SIZE - 1)) {
+	s->size = size;
+	s->items = (int *) calloc (sizeof(int), size);
+	s->top = -1;
+}	
+
+
+static void
+_free_stack(stack *s)
+{
+	if (s->top == -1) {
+		printf("\nEmpty stack. Nothing to deallocate\n");
+		return;
+	}
+	s->top = -1;
+	free(s->items);
+	s->items = NULL;
+	printf("\nStack deallocated\n");
+}
+
+static void
+//push(int elem)
+push(stack *s, int elem)
+{
+	//if (count_elem == (STACK_SIZE - 1)) {
+	if (s->top == (s->size - 1)) {
 		printf("\nStack is full. Overflow\n");
 		return;
 	}
-	array[++count_elem] = elem;
+	s->top += 1;
+	s->items[s->top] = elem;
+	//array[++count_elem] = elem;
 }
 
 static int
-pop(void)
+//pop(void)
+pop(stack *s)
 {
 	int elem = 0;
 
-	if (count_elem == -1) {
+	//if (count_elem == -1) {
+	if (s->top == -1) {
 		printf("\nEmpty stack. Underflow\n");
 		return -1;
 	}
-	elem = array[count_elem];
-	count_elem--;
+	//elem = array[count_elem];
+	elem = s->items[s->top];
+	//count_elem--;
+	s->top -= 1;
 	return elem;
 }
 
 static void
-display(void)
+//display(void)
+display(stack *s)
 {
-	int i = 0;
+	int i = s->top;
 
 	printf("\nSTACK\n");
-	while (i <= count_elem)
-		printf("%d ", array[i++]);
+	//while (i <= count_elem)
+	while (i >= 0)
+		//printf("%d ", array[i++]);
+		printf("%d ", s->items[i--]);
 }
 
 int 
@@ -48,7 +89,12 @@ main (int argc, char *argv[])
         int choice;
         int data;
 	int value;
+	int size = 0;
+	stack s;
 
+	printf("\nEnter the capacity of the stack\n");
+	scanf(" %d", &size);
+	_init_stack(&s, size);
         while (1) {
                 printf("\nStack\n\n");
                 printf("1. Push\n");
@@ -61,15 +107,14 @@ main (int argc, char *argv[])
                         case 1:
                                 printf("\nEnter an integer\n");
                                 scanf(" %d", &value);
-                                push(value);
+				push(&s, value);
                                 break;
                         case 2:
-				data = pop();
-				if (data >= 0)
-					printf("\nPopped element from the stack is %d\n", data);
+				data = pop(&s);
+				printf("\nPopped element from the stack is %d\n", data);
                                 break;
                         case 3:
-				display();
+				display(&s);
                                 break;
                         default:
                                 printf("\nWrong choice. Doing nothing\n");
@@ -81,5 +126,6 @@ main (int argc, char *argv[])
                         break;
                 }
         }
+	_free_stack(&s);
 	return 0;
 }
