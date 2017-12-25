d5d3d6538fc47598ae93d113540f6e25c44d4a9c
Nishanth Nagendra <nishanth.amogh@gmail.com>
2017-12-25 22:34:21 +0530

    Common matrix library

--- c/matrix/count_paths/driver.c
+++ c/matrix/count_paths/driver.c
@@ -3,6 +3,44 @@
 #include <string.h>
 #include "matrix.h"
 
+static int
+countPaths (MATRIX *mat)
+{
+    int **ptr = create2DArray(mat->rows, mat->cols);
+    int i, j;
+    int rows = mat->rows;
+    int cols = mat->cols;
+
+    if (!ptr)
+    {
+        printf("\nDue to memory allocation failure, count of paths is 0\n");
+        exit(0);
+    }
+    for (i = 0; i < cols; i++)
+    {
+        ptr[0][i] = 1;
+    }
+    for (i = 0; i < rows; i++)
+    {
+        ptr[i][0] = 1;
+    }
+    for (i = 1; i < rows; i++)
+    {
+       for (j = 1; j < cols; j++)
+       {
+          ptr[i][j] = ptr[i][j - 1] + ptr[i - 1][j];
+       }
+    }
+    printf("\nMatrix computed for counting paths\n\n");
+    MATRIX mat1;
+    mat1.rows = rows;
+    mat1.cols = cols;
+    mat1.count_paths = 0;
+    mat1.ptr = ptr;
+    displayMatrix(&mat1);
+    mat1.count_paths = ptr[rows - 1][cols - 1];
+}
+
 int main (int argc, char *argv[])
 {
     int rows, cols;
